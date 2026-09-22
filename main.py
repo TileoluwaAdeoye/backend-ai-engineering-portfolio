40e9ecc5b85dc1024702dc8b083bc4677cfe7b80
Tileoluwa Adeoye <150630399+TileoluwaAdeoye@users.noreply.github.com>
2026-09-22 21:57:35 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+#Building a very basic calculator
+#Create two variables
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+#You can use int or float, but int is limited, and float allows you to add an int and a float
+print(result) 
